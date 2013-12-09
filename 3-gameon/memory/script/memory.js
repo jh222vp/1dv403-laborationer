a84ae05bb9335db37f07fdc9ce5671abf9f91592
OpenShift guest <jh222vp@student.lnu.se>
2013-12-09 14:00:57 -0500

    lägger till lite variabler

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -4,13 +4,21 @@
 var Memory = {
     
     imageArray: [],
-    init : funtction(){
+    init : function(){
         //Deklarerar variabler
         
         //Dessa två variabler håller i hur många memorybrickor det är i höjd och sidled.
-        var column = 4;
+        var cols = 4;
         var rows = 4;
         
+        //Slumpar fram siffrorna med hjälp av random.js
+        Memory.imageArray = RandomGenerator.getPictureArray(cols, rows);
+        
+        /*for (var i = 0; i < Memory.imageArray.length; i++) {
+			Memory.imageArray[i] = "img/" + Memory.imageArray[i] + ".png";
+			renderBoard(i); //Skapa en LI tagg för varje bild.
+        }*/
+        console.log(Memory.imageArray);
     }
-    
 };
+window.onload = Memory.init; 
