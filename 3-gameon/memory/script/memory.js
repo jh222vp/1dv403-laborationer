b011214591d4fcd6a1843eedcbfe921f1176eec7
OpenShift guest <jh222vp@student.lnu.se>
2013-12-09 11:44:33 -0500

    lägger till lite variabler

---
+++ 3-gameon/memory/script/memory.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//Statiskt object
+var Memory = {
+    
+    imageArray: [],
+    init : funtction(){
+        //Deklarerar variabler
+        
+        //Dessa två variabler håller i hur många memorybrickor det är i höjd och sidled.
+        var column = 4;
+        var rows = 4;
+        
+    }
+    
+};
