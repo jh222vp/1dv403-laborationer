8f88d5fb85a1023ff64f58681f8f7b6c80553a86
OpenShift guest <jh222vp@student.lnu.se>
2014-01-09 09:25:50 -0500

    lagt in ajax, som hämtar bilderna, en for loop som loopar genom bilderna som är hämtade

--- 5-pwd/script.js
+++ 5-pwd/script.js
@@ -4,6 +4,7 @@
 var desktop = {
         count: 0,
     
+    
     //Skapar popup function
     popUpWindow: function(){
         this.count++;
@@ -28,6 +29,63 @@ var desktop = {
         cancelButton.appendChild(cancelButtonText);
         body.insertBefore(body.firstChild);
         
+        //Ajax Koden nedan:
+         var xhr = new XMLHttpRequest();
+         
+         //onreadystatechange == vad som ska hända när vi får svar från funktionen
+         //finns 4 olika koder. 4an betyder att allt är färdigt och "OK"
+         xhr.onreadystatechange = function(){
+             if(xhr.readyState === 4) {
+                 if(xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
+                    //alert(xhr.responseText);
+                    var imgWidth = 0;
+                    var imgHeight = 0;
+                    var jsonImageReciver = JSON.parse(xhr.responseText);
+                    //console.log(xhr.responseText);
+                  
+                    for (var i = 0; i < jsonImageReciver.length; i++) {
+                        
+                        //IF satserna kontrollerar så att ramen runt bilderna alltid är maxstora
+                        if (jsonImageReciver[i].thumbWidth > imgWidth){
+                            imgWidth = Math.max(jsonImageReciver[i].thumbWidth);
+                        }
+                        if (jsonImageReciver[i].thumbHeight > imgHeight){
+                            imgWidth = Math.max(jsonImageReciver[i].thumbHeight);
+                        }
+
+                        var thumbImgContainer = jsonImageReciver[i].thumbURL;
+                        console.log(thumbImgContainer);
+                    }
+                   
+                   //Array med alla bilder, HD och thumbs
+                    var jsonArray = new Array(jsonImageReciver);
+                    console.log(jsonArray);
+                    
+                    //popUp.appendchild();
+                    
+                 }
+                 //Else satsen skriver ut vad som inte gick rätt (allt utom 4)
+                 else{
+                     console.log("Läsfel, status:"+xhr.status);
+                 }
+             }
+         };
+         
+         //Länken där man hämtar bilderna
+         xhr.open("get", "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/", true);
+        xhr.send(null);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         //Tar bort popup fönstret
         cancelButton.onclick = function() {
             popUp.parentNode.removeChild(popUp);
@@ -50,7 +108,7 @@ var desktop = {
                     
                     console.log("click");
 
-            },false)
+            },false);
         },
 };
 
